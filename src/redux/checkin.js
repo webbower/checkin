a495c9dda5a7aafbe668a8a1f991ffa090b55672
Matt Bower <matt@webbower.com>
2020-05-27 17:56:51 -0700

    Create Redux state for teams with tests

--- src/redux/checkin.js
+++ src/redux/checkin.js
@@ -3,6 +3,13 @@ const createUser = ({ id = 0, name = 'John Doe' } = {}) => ({
   id,
   name,
 });
+
+const createTeam = ({ id = 0, name = 'Team 1', ownerId = 1, users: [] } = {}) => ({
+  id,
+  name,
+  ownerId,
+  users,
+});
 */
 export const addUser = ({ id = '0', name = 'Anonymous' } = {}) => ({
   type: 'checkin/ADD_USER',
@@ -12,9 +19,21 @@ export const addUser = ({ id = '0', name = 'Anonymous' } = {}) => ({
   },
 });
 
+export const addTeam = ({ id = '0', name = 'Team 1', ownerId = '0', users = [] } = {}) => ({
+  type: 'checkin/ADD_TEAM',
+  payload: {
+    id,
+    name,
+    ownerId,
+    // Add the creating user to the team along with anyone else, but avoid duplicates
+    users: [...new Set([ownerId, ...users])],
+  },
+});
+
 // Initial State
-export const getInitialState = ({ users = {} } = {}) => ({
+export const getInitialState = ({ users = {}, teams = {} } = {}) => ({
   users,
+  teams,
 });
 
 // Reducer
@@ -25,6 +44,20 @@ export default function reducer(state = getInitialState(), action = {}) {
         ...state,
         users: {
           ...state.users,
+          [action.payload.id]: action.payload,
+        },
+      };
+    case addTeam().type:
+      // If the owner doesn't exist, don't modify the state
+      // TODO Show error message
+      if (!state.users[action.payload.ownerId]) {
+        return state;
+      }
+
+      return {
+        ...state,
+        teams: {
+          ...state.teams,
           [action.payload.id]: action.payload,
         },
       };
--- src/redux/checkin.test.js
+++ src/redux/checkin.test.js
@@ -1,18 +1,26 @@
 import { describe } from 'riteway';
-import { default as reducer, addUser } from './checkin.js';
+import { default as reducer, addUser, addTeam } from './checkin.js';
 
 // TODO 26-05
-// - Replace `expected: getInitialState()` with test-only `createState()` factory
-// - Replace actionCreator().payload references in `expected` keys with test shape creators
-// - Principle of Least Knowledge!! (Need to know basis)
-// - Don't refactor out state slices. State is not complex enough.
+// √ Replace `expected: getInitialState()` with test-only `createTestState()` factory
+// √ Replace actionCreator().payload references in `expected` keys with test shape creators
+// √ Principle of Least Knowledge!! (Need to know basis)
+// √ Don't refactor out state slices. State is not complex enough.
 // - Action creators can conditionally dispatch one of multiple possible actions
 
-const createTestState = ({ users = {} } = {}) => ({
+const createTestState = ({ users = {}, teams = {} } = {}) => ({
   users,
+  teams,
 });
 
 const createTestUser = ({ id = '0', name = 'Anonymous' } = {}) => ({ id, name });
+
+const createTestTeam = ({ id = '0', name = 'Team 1', ownerId = '0', users = [] } = {}) => ({
+  id,
+  name,
+  ownerId,
+  users,
+});
 
 describe('checkin: reducer()', async (assert) => {
   assert({
@@ -44,6 +52,77 @@ describe('checkin: adding users', async (assert) => {
         '1': createTestUser({ id: '1' }),
         '2': createTestUser({ id: '2' }),
         '3': createTestUser({ id: '3' }),
+      },
+    }),
+  });
+});
+
+describe('checkin: adding teams', async (assert) => {
+  const owner = createTestUser({ id: '1' });
+
+  // TODO Should addTeams() add the team if users[ownerId] doesn't exist?
+  assert({
+    given: 'an addTeam action when the owner does not exist',
+    should: 'not modify the state',
+    actual: reducer(reducer(), addTeam({ id: '1', ownerId: owner.id })),
+    expected: createTestState(),
+  });
+
+  assert({
+    given: 'an addTeam action with an existing owner',
+    should: 'add the team and add the user who created the team to the new team',
+    actual: [addUser({ ...owner }), addTeam({ id: '1', ownerId: owner.id })].reduce(
+      reducer,
+      reducer()
+    ),
+    expected: createTestState({
+      users: {
+        [owner.id]: {
+          id: owner.id,
+          name: owner.name,
+        },
+      },
+      teams: {
+        '1': createTestTeam({
+          id: '1',
+          ownerId: owner.id,
+          users: [owner.id],
+        }),
+      },
+    }),
+  });
+
+  assert({
+    given: 'multiple addTeam actions with an existing owner',
+    should: 'add all teams to the state with the owner assigned to each team',
+    actual: ['1', '2', '3']
+      .map((id) => addTeam({ id, ownerId: owner.id }))
+      .reduce(reducer, reducer(undefined, addUser({ ...owner }))),
+    expected: createTestState({
+      users: {
+        [owner.id]: owner,
+      },
+      teams: {
+        '1': createTestTeam({ id: '1', ownerId: owner.id, users: [owner.id] }),
+        '2': createTestTeam({ id: '2', ownerId: owner.id, users: [owner.id] }),
+        '3': createTestTeam({ id: '3', ownerId: owner.id, users: [owner.id] }),
+      },
+    }),
+  });
+
+  assert({
+    given: 'adding a new team with initial users, repeating the ownerId in the users array',
+    should: 'only have the owner id appear once in team.users',
+    actual: [
+      addUser({ ...owner }),
+      addTeam({ id: '1', ownerId: owner.id, users: [owner.id, '2'] }),
+    ].reduce(reducer, reducer()),
+    expected: createTestState({
+      users: {
+        [owner.id]: owner,
+      },
+      teams: {
+        '1': createTestTeam({ id: '1', ownerId: owner.id, users: [owner.id, '2'] }),
       },
     }),
   });
