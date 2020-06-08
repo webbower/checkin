d2174dadc65474e58b79f9fa605b62c8494b101a
Matt Bower <matt@webbower.com>
2020-06-08 12:58:57 -0700

    Add React Router and integrate Users page into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
+
+import { UsersPage } from './components/users-page';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, World!</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/users.html">Users</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/users.html">
+              <UsersPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
