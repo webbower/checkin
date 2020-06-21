1002bf1d6bab6b4d1591ef792af28fffe29ec53a
Matt Bower <matt@webbower.com>
2020-06-21 15:59:10 -0700

    Reconfigure unit test setup
    - Create separate dev and prod lint and test commands
    - Create central tests file for dev to allow test disabling

---
+++ src/tests.dev.js
@@ -0,0 +1,12 @@
+import './utils/join-on.test.js';
+import './utils/pipe.test.js';
+import './utils/prop.test.js';
+
+import './redux/checkin.test.js';
+
+import './components/heading/heading.test.jsx';
+
+import './pages/users-page/users-page.test.jsx';
+import './pages/teams-page/teams-page.test.jsx';
+import './pages/checkins-index-page/checkins-index-page.test.jsx';
+import './pages/team-checkins-page/team-checkins-page.test.jsx';
